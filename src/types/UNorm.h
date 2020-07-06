faff8d92391ca2f604b18cfa45a35d4f3d8c823f
duhone <duhone@outlook.com>
2020-07-05 20:18:24 -0700

    finish UNorm

--- src/types/UNorm.h
+++ src/types/UNorm.h
@@ -1,5 +1,7 @@
 ﻿#include <core/Log.h>
 
+#include <3rdParty/glm.h>
+
 #include <limits>
 #include <numeric>
 #include <type_traits>
@@ -12,11 +14,7 @@ class UNorm {
 
   public:
 	UNorm() noexcept = default;
-	UNorm(float a_arg) noexcept {
-		a_arg = std::clamp(a_arg, 0.0f, 1.0f);
-		a_arg *= s_toT;
-		m_data = static_cast<T>(a_arg);
-	}
+	UNorm(float a_arg) noexcept { m_data = ToUnorm(a_arg); }
 	~UNorm() noexcept            = default;
 	UNorm(const UNorm&) noexcept = default;
 	UNorm(UNorm&&) noexcept      = default;
@@ -25,6 +23,10 @@ class UNorm {
 
 	operator float() const { return ToFloat(m_data); }
 	// spaceship
+
+	inline friend bool operator==(const UNorm<T>& a_arg1, const UNorm<T>& a_arg2) {
+		return a_arg1.m_data == a_arg2.m_data;
+	}
 
   private:
 	T ToUnorm(float a_arg) const noexcept {
@@ -41,12 +43,136 @@ class UNorm {
 	T m_data;
 };
 
+template<typename T>
+class UNorm2 {
+	friend std::hash<UNorm2<T>>;
+	static_assert(std::is_unsigned_v<T> && std::is_integral_v<T> && sizeof(T) <= 2,
+	              "UNorm template parameter must be an unsiged int, and 16 bits or less");
+
+  public:
+	UNorm2() noexcept = default;
+	UNorm2(glm::vec2 a_arg) noexcept {
+		m_data[0] = a_arg[0];
+		m_data[1] = a_arg[1];
+	}
+	UNorm2(float a_arg1, float a_arg2) noexcept {
+		m_data[0] = a_arg1;
+		m_data[1] = a_arg2;
+	}
+	~UNorm2() noexcept             = default;
+	UNorm2(const UNorm2&) noexcept = default;
+	UNorm2(UNorm2&&) noexcept      = default;
+	UNorm2& operator=(const UNorm2&) noexcept = default;
+	UNorm2& operator=(UNorm2&&) noexcept = default;
+
+	operator glm::vec2() const { return {m_data[0], m_data[1]}; }
+	// spaceship
+
+	inline friend bool operator==(const UNorm2<T>& a_arg1, const UNorm2<T>& a_arg2) {
+		return a_arg1.m_data[0] == a_arg2.m_data[0] && a_arg1.m_data[1] == a_arg2.m_data[1];
+	}
+
+  private:
+	UNorm<T> m_data[2];
+};
+
+template<typename T>
+class UNorm3 {
+	friend std::hash<UNorm3<T>>;
+	static_assert(std::is_unsigned_v<T> && std::is_integral_v<T> && sizeof(T) <= 2,
+	              "UNorm template parameter must be an unsiged int, and 16 bits or less");
+
+  public:
+	UNorm3() noexcept = default;
+	UNorm3(glm::vec3 a_arg) noexcept {
+		m_data[0] = a_arg[0];
+		m_data[1] = a_arg[1];
+		m_data[2] = a_arg[2];
+	}
+	UNorm3(float a_arg1, float a_arg2, float a_arg3) noexcept {
+		m_data[0] = a_arg1;
+		m_data[1] = a_arg2;
+		m_data[2] = a_arg3;
+	}
+	~UNorm3() noexcept             = default;
+	UNorm3(const UNorm3&) noexcept = default;
+	UNorm3(UNorm3&&) noexcept      = default;
+	UNorm3& operator=(const UNorm3&) noexcept = default;
+	UNorm3& operator=(UNorm3&&) noexcept = default;
+
+	operator glm::vec3() const { return {m_data[0], m_data[1], m_data[2]}; }
+	// spaceship
+
+	inline friend bool operator==(const UNorm3<T>& a_arg1, const UNorm3<T>& a_arg2) {
+		return a_arg1.m_data[0] == a_arg2.m_data[0] && a_arg1.m_data[1] == a_arg2.m_data[1] &&
+		       a_arg1.m_data[2] == a_arg2.m_data[2];
+	}
+
+  private:
+	UNorm<T> m_data[3];
+};
+
+template<typename T>
+class UNorm4 {
+	friend std::hash<UNorm4<T>>;
+	static_assert(std::is_unsigned_v<T> && std::is_integral_v<T> && sizeof(T) <= 2,
+	              "UNorm template parameter must be an unsiged int, and 16 bits or less");
+
+  public:
+	UNorm4() noexcept = default;
+	UNorm4(glm::vec4 a_arg) noexcept {
+		m_data[0] = a_arg[0];
+		m_data[1] = a_arg[1];
+		m_data[2] = a_arg[2];
+		m_data[3] = a_arg[3];
+	}
+	UNorm4(float a_arg1, float a_arg2, float a_arg3, float a_arg4) noexcept {
+		m_data[0] = a_arg1;
+		m_data[1] = a_arg2;
+		m_data[2] = a_arg3;
+		m_data[3] = a_arg4;
+	}
+	~UNorm4() noexcept             = default;
+	UNorm4(const UNorm4&) noexcept = default;
+	UNorm4(UNorm4&&) noexcept      = default;
+	UNorm4& operator=(const UNorm4&) noexcept = default;
+	UNorm4& operator=(UNorm4&&) noexcept = default;
+
+	operator glm::vec4() const { return {m_data[0], m_data[1], m_data[2], m_data[3]}; }
+	// spaceship
+
+	inline friend bool operator==(const UNorm4<T>& a_arg1, const UNorm4<T>& a_arg2) {
+		return a_arg1.m_data[0] == a_arg2.m_data[0] && a_arg1.m_data[1] == a_arg2.m_data[1] &&
+		       a_arg1.m_data[2] == a_arg2.m_data[2] && a_arg1.m_data[3] == a_arg2.m_data[3];
+	}
+
+  private:
+	UNorm<T> m_data[4];
+};
+
 namespace std {
 	template<typename T>
 	struct hash<UNorm<T>> {
-		std::size_t operator()(UNorm<T> const& a_arg) const noexcept {
-			std::size_t h1 = std::hash<T>{}(a_arg.m_data);
-			return h1;
+		std::size_t operator()(UNorm<T> const& a_arg) const noexcept { return std::hash<T>{}(a_arg.m_data); }
+	};
+	template<typename T>
+	struct hash<UNorm2<T>> {
+		std::size_t operator()(UNorm2<T> const& a_arg) const noexcept {
+			return std::hash<T>{}(a_arg.m_data[0]) ^ (std::hash<T>{}(a_arg.m_data[1]) << 1);
+		}
+	};
+	template<typename T>
+	struct hash<UNorm3<T>> {
+		std::size_t operator()(UNorm3<T> const& a_arg) const noexcept {
+			return std::hash<T>{}(a_arg.m_data[0]) ^ (std::hash<T>{}(a_arg.m_data[1]) << 1) ^
+			       (std::hash<T>{}(a_arg.m_data[2]) << 2);
+		}
+	};
+	template<typename T>
+	struct hash<UNorm4<T>> {
+		std::size_t operator()(UNorm4<T> const& a_arg) const noexcept {
+			return std::hash<T>{}(a_arg.m_data[0]) ^ (std::hash<T>{}(a_arg.m_data[1]) << 1) ^
+			       (std::hash<T>{}(a_arg.m_data[2]) << 2) ^ (std::hash<T>{}(a_arg.m_data[3]) << 3);
 		}
 	};
 }    // namespace std
--- tests/UNorm.cpp
+++ tests/UNorm.cpp
@@ -6,7 +6,7 @@
 
 namespace {
 	template<typename T>
-	void Test() {
+	void TestUNorm() {
 		float epsilon = 1.0f / std::numeric_limits<T>::max();
 
 		UNorm<T> unorm{0.5f};
@@ -28,9 +28,90 @@ namespace {
 		UNorm<T> unorm2 = unorm;
 		CHECK(unorm == unorm);
 	}
+
+	template<typename T>
+	void TestUNorm2() {
+		float epsilon = 1.0f / std::numeric_limits<T>::max();
+
+		UNorm2<T> unorm{0.5f, 0.35};
+		CHECK(((glm::vec2)unorm).x == doctest::Approx(0.5f).epsilon(epsilon));
+		CHECK(((glm::vec2)unorm).y == doctest::Approx(0.35f).epsilon(epsilon));
+
+		unorm = {0.0f, 0.0f};
+		CHECK(((glm::vec2)unorm).x == doctest::Approx(0.0f).epsilon(epsilon));
+		CHECK(((glm::vec2)unorm).y == doctest::Approx(0.0f).epsilon(epsilon));
+
+		// always clamped to 0-1
+		unorm = {-1.2f, 1.2f};
+		CHECK(((glm::vec2)unorm).x == doctest::Approx(0.0f).epsilon(epsilon));
+		CHECK(((glm::vec2)unorm).y == doctest::Approx(1.0f).epsilon(epsilon));
+
+		UNorm2<T> unorm2 = unorm;
+		CHECK(unorm == unorm);
+	}
+
+	template<typename T>
+	void TestUNorm3() {
+		float epsilon = 1.0f / std::numeric_limits<T>::max();
+
+		UNorm3<T> unorm{0.5f, 0.35, 0.1};
+		CHECK(((glm::vec3)unorm).x == doctest::Approx(0.5f).epsilon(epsilon));
+		CHECK(((glm::vec3)unorm).y == doctest::Approx(0.35f).epsilon(epsilon));
+		CHECK(((glm::vec3)unorm).z == doctest::Approx(0.1f).epsilon(epsilon));
+
+		unorm = {0.0f, 0.0f, 0.0f};
+		CHECK(((glm::vec3)unorm).x == doctest::Approx(0.0f).epsilon(epsilon));
+		CHECK(((glm::vec3)unorm).y == doctest::Approx(0.0f).epsilon(epsilon));
+		CHECK(((glm::vec3)unorm).z == doctest::Approx(0.0f).epsilon(epsilon));
+
+		// always clamped to 0-1
+		unorm = {-1.2f, 1.2f, -0.2f};
+		CHECK(((glm::vec3)unorm).x == doctest::Approx(0.0f).epsilon(epsilon));
+		CHECK(((glm::vec3)unorm).y == doctest::Approx(1.0f).epsilon(epsilon));
+		CHECK(((glm::vec3)unorm).z == doctest::Approx(0.0f).epsilon(epsilon));
+
+		UNorm3<T> unorm2 = unorm;
+		CHECK(unorm == unorm);
+	}
+
+	template<typename T>
+	void TestUNorm4() {
+		float epsilon = 1.0f / std::numeric_limits<T>::max();
+
+		UNorm4<T> unorm{0.5f, 0.35, 0.1, 0.9f};
+		CHECK(((glm::vec4)unorm).x == doctest::Approx(0.5f).epsilon(epsilon));
+		CHECK(((glm::vec4)unorm).y == doctest::Approx(0.35f).epsilon(epsilon));
+		CHECK(((glm::vec4)unorm).z == doctest::Approx(0.1f).epsilon(epsilon));
+		CHECK(((glm::vec4)unorm).w == doctest::Approx(0.9f).epsilon(epsilon));
+
+		unorm = {0.0f, 0.0f, 0.0f, 0.0f};
+		CHECK(((glm::vec4)unorm).x == doctest::Approx(0.0f).epsilon(epsilon));
+		CHECK(((glm::vec4)unorm).y == doctest::Approx(0.0f).epsilon(epsilon));
+		CHECK(((glm::vec4)unorm).z == doctest::Approx(0.0f).epsilon(epsilon));
+		CHECK(((glm::vec4)unorm).w == doctest::Approx(0.0f).epsilon(epsilon));
+
+		// always clamped to 0-1
+		unorm = {-1.2f, 1.2f, -0.2f, 0.7f};
+		CHECK(((glm::vec4)unorm).x == doctest::Approx(0.0f).epsilon(epsilon));
+		CHECK(((glm::vec4)unorm).y == doctest::Approx(1.0f).epsilon(epsilon));
+		CHECK(((glm::vec4)unorm).z == doctest::Approx(0.0f).epsilon(epsilon));
+		CHECK(((glm::vec4)unorm).w == doctest::Approx(0.7f).epsilon(epsilon));
+
+		UNorm4<T> unorm2 = unorm;
+		CHECK(unorm == unorm);
+	}
 }    // namespace
 
 TEST_CASE("unorm") {
-	Test<uint8_t>();
-	Test<uint16_t>();
+	TestUNorm<uint8_t>();
+	TestUNorm<uint16_t>();
+
+	TestUNorm2<uint8_t>();
+	TestUNorm2<uint16_t>();
+
+	TestUNorm3<uint8_t>();
+	TestUNorm3<uint16_t>();
+
+	TestUNorm4<uint8_t>();
+	TestUNorm4<uint16_t>();
 }
